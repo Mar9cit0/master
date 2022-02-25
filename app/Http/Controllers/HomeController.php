efa3e8fcd17dda4f5fdae8dfb62502d8db01c0ea
Mar9cit0 <84685205+Mar9cit0@users.noreply.github.com>
2022-02-25 05:13:18 -0300

    frontend

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $total = User::count();
+        return view('dashboard')->with("total",$total);
+    }
+}
